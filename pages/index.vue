888063c7356e7a64d6cc9cdbc9d7eace529f9335
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2023-11-26 20:58:39 +0700

    Created a Rest API and output a test request

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1 class="text-3xl font-thin">Hello world</h1>
+    <h1 class="text-3xl">
+        {{ data }}
+    </h1>
 </template>
+
+<script setup lang="ts">
+import type { BlogModel } from '../server/model/blog';
+
+const data = ref<BlogModel[]>([]);
+
+const fetchData = async () => {
+    try {
+        const result = await $fetch('/api/blog');
+        data.value = result.data as BlogModel[];
+    } catch (error) {
+        alert('Error')
+    }
+}
+onMounted(fetchData);
+</script>
